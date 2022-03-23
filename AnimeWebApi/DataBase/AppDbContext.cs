72f1b0cfd18d31c2051832dcc577fb07c104b4a3
Basit Mir <42338600+basitmir2020@users.noreply.github.com>
2022-03-23 17:38:46 +0530

    Add project files.

---
+++ AnimeWebApi/DataBase/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AnimeWebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeWebApi.DataBase
+{
+    public class AppDbContext : IdentityDbContext<ApiUser>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
